c4661902e652a7c31d09f0115454c54a15497763
mxy <13614169547@163.com>
2023-06-09 01:10:28 +0800

    删除boost

--- main.cxx
+++ main.cxx
@@ -1,66 +1,68 @@
-#include "http_helper.hxx"
-#include <boost/beast/http/status.hpp>
+#include <algorithm>
+#include <arpa/inet.h>
 #include <cstddef>
 #include <limits.h>
 #include <netinet/in.h>
+#include <ostream>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <string>
 #include <sys/socket.h>
 #include <unistd.h>
-
-#include <boost/beast.hpp>
-#include <boost/lexical_cast.hpp>
 #include <iostream>
 #include <memory>
 #include <sstream>
+#include <ranges>
+#include <vector>
 
-#define PORT 8848
+#define PORT 8082
 #define MAX_HTTP_LEN 2048
 
-void* handle_connection(void* arg) {
+void *handle_connection(void *arg) {
   const int new_socket_fd = (long long)arg;
-  namespace beast = boost::beast;
-  namespace http = beast::http;
-  using u8 = unsigned char;
-  const auto buffer = std::make_unique<u8[]>(MAX_HTTP_LEN);
-  std::fill(buffer.get(), buffer.get() + MAX_HTTP_LEN, (u8)0);
+  auto buffer = std::string(MAX_HTTP_LEN, 0);
   while (true) {
     const size_t bytes_recieved =
-        recv(new_socket_fd, buffer.get(), MAX_HTTP_LEN, 0);
+        recv(new_socket_fd, buffer.data(), MAX_HTTP_LEN, 0);
     // 客户端关闭了tcp连接
+    std::cout << "recv: " << buffer.substr(0, bytes_recieved) << std::endl;
     if (bytes_recieved == 0) {
+      std::cout << "closed" << std::endl;
       pthread_exit(NULL);
     }
-    http::request_parser<http::string_body> parser{};
-    beast::error_code ec;
-    parser.put({buffer.get(), bytes_recieved}, ec);
-    if (parser.is_done()) {
-      auto req = parser.get();
-      std::cout << "HTTP Method: " << req.method_string() << std::endl;
-      std::cout << "Request Target: " << req.target() << std::endl;
-      std::cout << "HTTP Version: " << req.version() << std::endl;
 
-      // 打印HTTP头部字段
-      for (const auto& field : req.base()) {
-        std::cout << field.name_string() << ": " << field.value() << std::endl;
-      }
-
-      // 打印HTTP消息体
-      std::cout << "Message Body: " << req.body() << std::endl;
-
-      // 返回http response
-      const auto response = mxy::http_helper::make_response(req);
-      write(new_socket_fd, response.data(), response.size());
-    } else {
-      std::cerr << "Failed to parse HTTP request" << std::endl;
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(8080);
+    inet_pton(AF_INET, "8.130.13.195", &(addr.sin_addr));
+    if (connect(fd, (sockaddr *)&addr, sizeof(addr)) == -1) {
+      perror("connect");
+      return nullptr;
     }
-    std::fill(buffer.get(), buffer.get() + MAX_HTTP_LEN, (u8)0);
+    
+    send(fd, buffer.data(), bytes_recieved, 0);
+
+    std::cout << "write to data-server: " << buffer.substr(0, bytes_recieved) << std::endl;
+
+    std::ranges::fill(buffer, 0);
+    const size_t bytes_from_data_server = recv(fd, buffer.data(), MAX_HTTP_LEN, 0);
+
+    std::cout << "recv from data-server: " << buffer.substr(0, bytes_from_data_server) << std::endl;
+
+    // write(new_socket_fd, buffer.data(), bytes_from_data_server);
+    send(new_socket_fd, buffer.data(), bytes_from_data_server, 0);
+
+    std::cout << "write back: " << buffer.substr(0, bytes_from_data_server) << std::endl;
+    std::ranges::fill(buffer, 0);
   }
 }
 
-int main(int argc, char const* argv[]) {
+int main(int argc, char const *argv[]) {
   sockaddr_in addr;
   memset(&addr, 0, sizeof(addr));
   addr.sin_family = AF_INET;
@@ -73,7 +75,7 @@ int main(int argc, char const* argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  if (bind(socket_fd, (sockaddr*)&addr, sizeof(addr)) == -1) {
+  if (bind(socket_fd, (sockaddr *)&addr, sizeof(addr)) == -1) {
     perror("bind");
     exit(EXIT_FAILURE);
   }
@@ -88,10 +90,10 @@ int main(int argc, char const* argv[]) {
     memset(&client_addr, 0, sizeof(client_addr));
     const auto addr_len = sizeof(client_addr);
     const auto new_socket_fd =
-        accept(socket_fd, (sockaddr*)&client_addr, (socklen_t*)&addr_len);
+        accept(socket_fd, (sockaddr *)&client_addr, (socklen_t *)&addr_len);
     pthread_t thread;
     if (pthread_create(&thread, NULL, handle_connection,
-                       (void*)new_socket_fd) != 0) {
+                       (void *)new_socket_fd) != 0) {
       perror("pthread_create");
       exit(EXIT_FAILURE);
     }
